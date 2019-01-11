e35d1575f583445328eb4d50b3182e19ede3ab89
primaananda <emailiputuprimaananda@gmail.com>
2019-01-11 11:51:42 +0800

    adding more

--- app.py
+++ app.py
@@ -9,38 +9,38 @@ from argparse import ArgumentParser
 from flask import Flask, request, abort
 
 from linebot import (
-	LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-	LineBotApiError, InvalidSignatureError
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
-	MessageEvent, TextMessage, TextSendMessage,
-	SourceUser, SourceGroup, SourceRoom,
-	TemplateSendMessage, ConfirmTemplate, MessageAction,
-	ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
-	PostbackAction, DatetimePickerAction,
-	CameraAction, CameraRollAction, LocationAction,
-	CarouselTemplate, CarouselColumn, PostbackEvent,
-	StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-	ImageMessage, VideoMessage, AudioMessage, FileMessage,
-	UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
-	FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
-	TextComponent, SpacerComponent, IconComponent, ButtonComponent,
-	SeparatorComponent, QuickReply, QuickReplyButton
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton
 )
 
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+#channel_secret = os.getenv('cba2b4b2e75c6d116878aa9b91825af4')
+#channel_access_token = os.getenv#('exFmH2PrUuHfOvyb4iF/HZ59hFP0R0uIu0N9gPld1A2kgfPr/2lAHqxnCzsok79m2UxBOLe9UvIDN41keSCAA1NRfYpcPC9ZkUHvHd0vaRdUtbWSA0rwuzMT0rR9TS4qXZunW9gaWR5/BXDj4yvG1AdB04t89/1O/w1cDnyilFU=')
+#if channel_secret is None:
+#    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+#    sys.exit(1)
+#if channel_access_token is None:
+#    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+#    sys.exit(1)
 
 line_bot_api = LineBotApi('exFmH2PrUuHfOvyb4iF/HZ59hFP0R0uIu0N9gPld1A2kgfPr/2lAHqxnCzsok79m2UxBOLe9UvIDN41keSCAA1NRfYpcPC9ZkUHvHd0vaRdUtbWSA0rwuzMT0rR9TS4qXZunW9gaWR5/BXDj4yvG1AdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('cba2b4b2e75c6d116878aa9b91825af4')
@@ -49,87 +49,87 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 # function for create tmp dir for download content
 def make_static_tmp_dir():
-	try:
-		os.makedirs(static_tmp_path)
-	except OSError as exc:
-		if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
-			pass
-		else:
-			raise
-			
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+            
 @app.route("/callback", methods=['POST'])
 def callback():
     # Get X-Line-Signature header value
-	signature = request.headers['X-Line-Signature']
+    signature = request.headers['X-Line-Signature']
 
     # Get request body as text
-	body = request.get_data(as_text=True)
-	app.logger.info("Request body: " + body)
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
-	try:
-		handler.handle(body, signature)
-	except LineBotApiError as e:
-		print("Got exception from LINE Messaging API: %s\n" % e.message)
-		for m in e.error.details:
-			print("  %s: %s" % (m.property, m.message))
-		print("\n")
-	except InvalidSignatureError:
-		abort(400)
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        print("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            print("  %s: %s" % (m.property, m.message))
+        print("\n")
+    except InvalidSignatureError:
+        abort(400)
 
-	return 'OK'
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-	text = event.message.text
-	
-	if text == 'profile':
-		if isinstance(event.source, SourceUser):
-			profile = line_bot_api.get_profile(event.source.user_id)
-			line_bot_api.reply_message(
-				event.reply_token, [
-					TextSendMessage(text='Display name: ' + profile.display_name),
-					TextSendMessage(text='Status message: ' + profile.status_message)
-				]
-			)
-		else:
-			line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text="Bot can't use profile API without user ID"))
-	elif text == 'bye':
-		if isinstance(event.source, SourceGroup):
-			line_bot_api.reply_message(
-				event.reply_token, TextSendMessage(text='Leaving group'))
-			line_bot_api.leave_group(event.source.group_id)
-		elif isinstance(event.source, SourceRoom):
-			line_bot_api.reply_message(
-				event.reply_token, TextSendMessage(text='Leaving group'))
-			line_bot_api.leave_room(event.source.room_id)
-		else:
-			line_bot_api.reply_message(
-				event.reply_token,
-				TextSendMessage(text="Bot can't leave from 1:1 chat"))
-	elif text == 'confirm':
-		confirm_template = ConfirmTemplate(text='Do it?', actions=[
-			MessageAction(label='Yes', text='Yes!'),
-			MessageAction(label='No', text='No!'),
+    text = event.message.text
+        
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text='Display name: ' + profile.display_name),
+                    TextSendMessage(text='Status message: ' + profile.status_message)
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Bot can't use profile API without user ID"))
+    elif text == 'bye':
+        if isinstance(event.source, SourceGroup):
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text='Leaving group'))
+            line_bot_api.leave_group(event.source.group_id)
+        elif isinstance(event.source, SourceRoom):
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text='Leaving group'))
+            line_bot_api.leave_room(event.source.room_id)
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Bot can't leave from 1:1 chat"))
+    elif text == 'confirm':
+        confirm_template = ConfirmTemplate(text='Do it?', actions=[
+            MessageAction(label='Yes', text='Yes!'),
+            MessageAction(label='No', text='No!'),
         ])
-		template_message = TemplateSendMessage(
-			alt_text='Confirm alt text', template=confirm_template)
-		line_bot_api.reply_message(event.reply_token, template_message)
-	elif text == 'buttons':
-		buttons_template = ButtonsTemplate(
-			title='My buttons sample', text='Hello, my buttons', actions=[
-				URIAction(label='Go to line.me', uri='https://line.me'),
-				PostbackAction(label='ping', data='ping'),
-				PostbackAction(label='ping with text', data='ping', text='ping'),
-				MessageAction(label='Translate Rice', text='米')
+        template_message = TemplateSendMessage(
+            alt_text='Confirm alt text', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'buttons':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
             ])
-		template_message = TemplateSendMessage(
-			alt_text='Buttons alt text', template=buttons_template)
-		line_bot_api.reply_message(event.reply_token, template_message)
-	elif text == 'carousel':
-		carousel_template = CarouselTemplate(columns=[
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'carousel':
+        carousel_template = CarouselTemplate(columns=[
             CarouselColumn(text='hoge1', title='fuga1', actions=[
                 URIAction(label='Go to line.me', uri='https://line.me'),
                 PostbackAction(label='ping', data='ping')
@@ -139,12 +139,12 @@ def handle_text_message(event):
                 MessageAction(label='Translate Rice', text='米')
             ]),
         ])
-		template_message = TemplateSendMessage(
+        template_message = TemplateSendMessage(
             alt_text='Carousel alt text', template=carousel_template)
-		line_bot_api.reply_message(event.reply_token, template_message)
-	elif text == 'image_carousel':
-		image_carousel_template = ImageCarouselTemplate(columns=[
-			ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'image_carousel':
+        image_carousel_template = ImageCarouselTemplate(columns=[
+            ImageCarouselColumn(image_url='https://via.placeholder.com/1024x1024',
                                 action=DatetimePickerAction(label='datetime',
                                                             data='datetime_postback',
                                                             mode='datetime')),
@@ -153,11 +153,12 @@ def handle_text_message(event):
                                                             data='date_postback',
                                                             mode='date'))
         ])
-		template_message = TemplateSendMessage(
+        template_message = TemplateSendMessage(
             alt_text='ImageCarousel alt text', template=image_carousel_template)
-		line_bot_api.reply_message(event.reply_token, template_message)
+        line_bot_api.reply_message(event.reply_token, template_message)
     elif text == 'imagemap':
         pass
+
     elif text == 'flex':
         bubble = BubbleContainer(
             direction='ltr',
