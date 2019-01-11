e5e58d6ed8a11a26d01ba9ee9c890967407a20da
primaananda <emailiputuprimaananda@gmail.com>
2019-01-11 11:03:02 +0800

    indent

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def handle_text_message(event):
         ])
 		template_message = TemplateSendMessage(
             alt_text='ImageCarousel alt text', template=image_carousel_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
+		line_bot_api.reply_message(event.reply_token, template_message)
     elif text == 'imagemap':
         pass
     elif text == 'flex':
