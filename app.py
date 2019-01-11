ab71ca5c3ab684f77d6c3fc6236742d5128d436b
primaananda <emailiputuprimaananda@gmail.com>
2019-01-11 16:16:17 +0800

    adding more

--- app.py
+++ app.py
@@ -316,8 +316,9 @@ def handle_text_message(event):
     elif text == 'ip':
         res = request.get('https://ipinfo.io/')
         data = res.json()
+        your_ip = data['ip']
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text='your ip ' + data['ip']))
+            event.reply_token, TextSendMessage(text='your ip ', quick_reply=your_ip))
     else:
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text='Need help? click this button .help', quick_reply=QuickReply(
