d7a290de375c9425f0b5f1aac479546480dcfe2b
primaananda <emailiputuprimaananda@gmail.com>
2019-01-21 16:56:50 +0800

    add info grapari

--- app.py
+++ app.py
@@ -125,8 +125,7 @@ def handle_text_message(event):
     elif text == 'navigation':
         buttons_template = ButtonsTemplate(
             title='Navigation Bot vers.1', text='Hello, ada yang bisa saya bantu...?', actions=[
-                MessageAction(label='Grapari Teuku Umar', text='cari info Grapari Teuku Umar'),
-                MessageAction(label='Grapari Renon', text='cari info Grapari Renon'),
+                MessageAction(label='Info Grapari', text='cari info Grapari'),
                 URIAction(label='About Dev.', uri='http://line.me/ti/p/~primaananda_')
             ])
         template_message = TemplateSendMessage(
@@ -135,7 +134,8 @@ def handle_text_message(event):
     elif text == 'cari info grapari' or text == 'cari grapari' or text == 'grapari':
         buttons_template = ButtonsTemplate(
             title='Navigation Bot vers.1', text='Hello, ada yang bisa saya bantu...?', actions=[
-                MessageAction(label='Info Grapari', text='cari info Grapari'),
+                MessageAction(label='Grapari Teuku Umar', text='cari info Grapari Teuku Umar'),
+                MessageAction(label='Grapari Renon', text='cari info Grapari Renon'),
                 URIAction(label='About Dev.', uri='http://line.me/ti/p/~primaananda_')
             ])
         template_message = TemplateSendMessage(
