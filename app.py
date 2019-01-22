55295ed3e9941574c0a0b2487becc54b2dbfc43a
primaananda <emailiputuprimaananda@gmail.com>
2019-01-22 16:56:25 +0800

    adding grapari

--- app.py
+++ app.py
@@ -126,9 +126,9 @@ def handle_text_message(event):
                 MessageAction(label='grapari teuku umar', text='grapari teuku umar'),
                 MessageAction(label='grapari renon', text='grapari renon')
             ]),
-            CarouselColumn(text='Daftar grapari wilayah Bangli', title='Bangli', actions=[
-                MessageAction(label='Grapari Not Found', text='asd'),
-            ]),
+            CarouselColumn(text='Daftar grapari wilayah Bangli', title='Bangli', actions=
+                MessageAction(label='Grapari Not Found', text='asd')
+            ),
             CarouselColumn(text='Daftar grapari wilayah Buleleng', title='Buleleng', actions=[
                 MessageAction(label='ping with text', text='ping'),
                 MessageAction(label='Translate Rice', text='米')
