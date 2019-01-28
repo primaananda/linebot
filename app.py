f47ae9307ea8034f3a1484b3806738653d0a4981
primaananda <emailiputuprimaananda@gmail.com>
2019-01-28 15:14:52 +0800

    adding postbackaction

--- app.py
+++ app.py
@@ -131,10 +131,8 @@ def handle_text_message(event):
                 MessageAction(label=' ', text=' ')
             ]),
             CarouselColumn(text='Daftar grapari wilayah Buleleng', title='Buleleng', actions=[
-                PostbackAction(label='Status Grapari', data='tidak terdapat Grapari di Bangli', text=' '),
+                MessageAction(label='grapari Buleleng', text='grapari buleleng'),
                 MessageAction(label=' ', text=' ')
-                # MessageAction(label='grapari Buleleng', text='grapari buleleng'),
-                # MessageAction(label=' ', text=' ')
             ]),
             CarouselColumn(text='Daftar grapari wilayah Gianyar', title='Gianyar', actions=[
                 PostbackAction(label='Status Grapari', data='tidak terdapat Grapari di Bangli', text=' '),
